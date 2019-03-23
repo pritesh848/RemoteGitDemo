c67f2906b9e2106c4ae727d05f0935b8b9e95d27
pritesh848 <pritesh848@gmail.com>
2019-03-23 13:19:10 +0530

    developer 2 commit

--- src/Hello.java
+++ src/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 System.out.println("hello git");
 System.out.println("second commit");
+System.out.println("chsnges by developer 2");
 	}
 
 }
